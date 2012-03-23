4fd6cee5c836d9ce4de4ee2fb48697ba260e8f04
Joakim Stenberg <joakim@saplo.com>
2012-03-23 11:51:29 +0100

    Fixed bug with incorrect path to Saplo library from collection.php

--- collection.php
+++ collection.php
@@ -1,12 +1,12 @@
 <?php
-
-require_once '../Saplo.php';
+require_once 'settings.php';
+require_once 'libraries/Saplo.php';
 
 /*
  * Create a Saplo collection where tweets can be stored.
  */
 
-$saplo = new Saplo();
+$saplo = new Saplo(API_KEY, SECRET_KEY);
 
 try
 {
@@ -17,11 +17,23 @@ try
 	
 	$response = $saplo->request('collection.create', $params);
 	
-	echo $response['collection_id'];
+	echo "Collection ID: " . $response['collection_id'];
 }
 catch (SaploException $e)
 {
-	echo "Saplo API error\n";
-	echo "Code: " . $e->getCode() . "\n";
-	echo "Message: " . $e->getMessage();
+	echo "<p>Saplo API error</p>";
+	echo "<p>Code: " . $e->getCode() . "</p>";
+	echo "<p>Message: " . $e->getMessage() . "</p>";
+	
+	if ($e->getCode() == 1203)
+	{
+		$response = $saplo->request('collection.list', array(), 'collections');
+		
+		echo "<p>List of collecions owned by this API account:</p>";
+				
+		foreach ($response as $collection)
+		{
+			echo $collection['collection_id'] . " (" . $collection['collection_name'] . ")";
+		}
+	}
 }
--- libraries/Saplo.php
+++ libraries/Saplo.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'Debug.php';
+
 /**
  * Saplo API
  * 
